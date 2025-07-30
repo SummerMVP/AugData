6a39f2484a880e4d0dd7d0548ff28d9e1927cfcd
SummerMVP <50560767+SummerMVP@users.noreply.github.com>
2025-07-30 11:14:34 +0800

    Add files via upload

---
+++ AugNet/tools/funcs.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import logging
+import random
+import torch
+import torch.nn as nn
+from tools.srm_filter_kernel import all_normalized_hpf_list
+from steganalyzer import SRNet,LWENet,CovNet,YedNet
+import torch.nn.functional as F
+
+# stego被误判为cover的交叉熵损失
+class ADVLoss(nn.Module):
+    def __init__(self,modelType,model_path):
+        super(ADVLoss, self).__init__()
+        self.modelType = modelType
+        self.model_path = model_path
+
+    def forward(self, Augdata,label):
+        if self.modelType == 'SRNet':
+            net = SRNet.SRNet().cuda()
+            net = nn.DataParallel(net)
+        elif self.modelType == 'CovNet':
+            net = CovNet.CovNet().cuda()
+        elif self.modelType == 'LWENet':
+            net = LWENet.lwenet().cuda()
+            net = nn.DataParallel(net)
+        net.load_state_dict(torch.load(self.model_path)['state_dict'], strict=True)
+        net.eval()
+        out = net(Augdata)
+        # 初始化L1损失函数
+        criterion = nn.CrossEntropyLoss()
+        # 计算L1损失
+        loss = criterion(out, label)
+        return loss
+
+class AverageMeter(object):
+  def __init__(self):
+    self.reset()
+
+  def reset(self):
+    self.val = 0
+    self.avg = 0
+    self.sum = 0
+    self.count = 0
+
+  def update(self, val, n=1):
+    self.val = val
+    self.sum += val * n
+    self.count += n
+    self.avg = self.sum / self.count
+
+
+#Initialization
+def initWeights(module):
+  if type(module) == nn.Conv2d:
+    if module.weight.requires_grad:
+      nn.init.kaiming_normal_(module.weight.data, mode='fan_in', nonlinearity='relu')
+
+  if type(module) == nn.Linear:
+    nn.init.normal_(module.weight.data, mean=0, std=0.01)
+    nn.init.constant_(module.bias.data, val=0)
+
+#Data augmentation 
+class AugData():
+  def __call__(self, sample):
+    data, label,name = sample['data'], sample['label'], sample['name']
+
+    #Rotation
+    rot = random.randint(0,3)
+    if len(data.shape)>=3:
+        data = np.rot90(data, rot, axes=[1, 2]).copy()
+    else:
+        data = np.rot90(data, rot, axes=[0, 1]).copy()
+    #Mirroring 
+    if random.random() < 0.5:
+        if len(data.shape) >= 3:
+            data = np.flip(data, axis=2).copy()
+        else:
+            data = np.flip(data, axis=-2).copy()
+
+    new_sample = {'data': data, 'label': label,'name':name}
+
+    return new_sample
+
+
+class ToTensor():
+  def __call__(self, sample):
+    data, label = sample['data'], sample['label']
+
+    data = np.expand_dims(data, axis=1)
+    data = data.astype(np.float32)
+    # data = data / 255.0
+
+    new_sample = {
+      'data': torch.from_numpy(data),
+      'label': torch.from_numpy(label).long(),
+      'name': sample['name']
+    }
+
+    return new_sample
+
+def setLogger(log_path, mode='a'):
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        # Logging to a file
+        file_handler = logging.FileHandler(log_path, mode=mode)
+        file_handler.setFormatter(logging.Formatter('%(asctime)s: %(message)s', '%Y-%m-%d %H:%M:%S'))
+        logger.addHandler(file_handler)
+
+        # Logging to console
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(logging.Formatter('%(message)s'))
+        logger.addHandler(stream_handler)
+
+
